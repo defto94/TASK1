db7d6184dbb71f076ce0d6a4249ac6f4d0d2472e
Ola <defto94@gmail.com>
2018-01-15 15:49:30 +0100

    Spring Security - small fixes, jQuery example added

--- src/main/java/pl/lodz/project/task1/config/WebSecurityConfig.java
+++ src/main/java/pl/lodz/project/task1/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home","/users","/books","/hello.js").permitAll()
+                .antMatchers("/", "/home","/users","/books","/hello.js","/deleteBook/{id}","/addBook","/updateBook","/delete/{id}","/add","/update").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
